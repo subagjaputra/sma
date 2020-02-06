44312ec7c990fcf16fcc7d9817f691d6b3b25e1c
subagjaputra <subagjaputra@gmail.com>
2020-02-06 18:30:08 +0700

    mailing-tobe

--- application/views/mailing.php
+++ application/views/mailing.php
@@ -13,17 +13,46 @@
 </head>
 
 <body>
-    <div class="container">
-        <div class="row">
+    <div class="container-fluid mt-3">
+        <div class="row mb-3">
             <div class="col-8">
-                MAP
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.1439134591315!2d106.92233691449226!3d-6.111318661635951!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a200bfdc75913%3A0x6cb5167b44813b00!2sSMA%20Negeri%2073%20Jakarta!5e0!3m2!1sid!2sid!4v1580986793502!5m2!1sid!2sid" width="800" height="400" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
             </div>
             <div class="col-4">
-                Address
+                <h3>Address</h3>
+                <p>SMAN 73 Jakarta
+                    Jl. Ardodedali No.6-7, RT.8/RW.2, Semper Tim
+                    Cilincing, Kota Jkt Utara, Daerah Khusus Ibukota Jakarta 14130
+                    website : sman73jkt.sch.id
+                    Telp : (021) 4401622</p>
+                <!-- <h3 style="margin-left:120px;">Contact Us</h3> -->
             </div>
         </div>
         <div class="row justify-content-center">
-            FORM
+            <div class="col-lg-6">
+                <h2 class="text-center">Contact Us</h2>
+                <form>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="inputEmail4">Firstname</label>
+                            <input type="text" class="form-control" id="inputEmail4">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="inputPassword4">Lastname</label>
+                            <input type="text" class="form-control" id="inputPassword4">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputAddress">Email</label>
+                        <input type="text" class="form-control" id="inputAddress">
+                    </div>
+                    <div class="form-group">
+                        <label for="inputAddress2">Address</label>
+                        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Send</button>
+                </form>
+            </div>
         </div>
     </div>
 
