ae95c91d51a6aeb6a718f19c8c24fe7ae656b614
subagjaputra <subagjaputra@gmail.com>
2020-02-07 15:44:13 +0700

    solv about email

--- application/controllers/Mailing.php
+++ application/controllers/Mailing.php
@@ -1,8 +1,14 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 // This will be my Work -> Putra
 
 class Mailing extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('email');
+    }
     public function index()
     {
         $this->load->view('mailing');
@@ -10,13 +16,11 @@ class Mailing extends CI_Controller
 
     public function send()
     {
-        $this->load->library('email');
-
         $config = [
             'protocol'  => 'smtp',
             'smtp_host' => 'ssl://smtp.googlemail.com',
             'smtp_user' => 'gerup.mail@gmail.com',
-            'smtp_pass' => '*********',
+            'smtp_pass' => 'gerupM@!L',
             'smtp_port' => 465,
             'mailtype'  => 'html',
             'charset'   => 'utf-8',
@@ -25,18 +29,33 @@ class Mailing extends CI_Controller
 
         $this->email->initialize($config);
 
-        $this->email->from('gerup.mail@gmail.com', 'Gerup Email');
-        $this->email->to('linglung.puyeng@gmail.com');
-
-        $this->email->subject('Email Test');
-        $this->email->message('Testing the email.');
+        $this->email->from($this->input->post('email'), $this->input->post('name'));
+        $this->email->to('gerup.mail@gmail.com');
+        $this->email->subject('Email From Website');
+        $this->email->message($this->input->post('message'));
 
         if ($this->email->send()) {
-            echo 'Sent';
+            $this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+            Your email has been sent. Please check your email.
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>');
+            $this->_noReply();
+            redirect('mailing');
             return true;
         } else {
             echo $this->email->print_debugger();
             die;
         }
+    }
+
+    public function _noReply()
+    {
+        $this->email->from('no-reply@sman73.sch.id', 'no-reply');
+        $this->email->to($this->input->post('email'));
+        $this->email->subject('no-reply');
+        $this->email->message('Thanks for your email. We will progress soon.');
+        return $this->email->send();
     }
 }
--- application/views/mailing.php
+++ application/views/mailing.php
@@ -31,24 +31,15 @@
         <div class="row justify-content-center">
             <div class="col-lg-6">
                 <h2 class="text-center">Contact Us</h2>
-                <form>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label for="firstname">Firstname</label>
-                            <input type="text" class="form-control" id="firstname" name="firstname">
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label for="lastname">Lastname</label>
-                            <input type="text" class="form-control" id="lastname" name="lastname">
-                        </div>
+                <?= $this->session->flashdata('message'); ?>
+                <form action="<?= base_url('mailing/send') ?>" method="post">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Your Name">
                     </div>
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" name="email">
-                    </div>
-                    <div class="form-group">
-                        <label for="address">Address</label>
-                        <input type="text" class="form-control" id="address" name="address" placeholder="Apartment, studio, or floor">
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Your Email">
                     </div>
                     <div class="form-group">
                         <label for="message">Message</label>
